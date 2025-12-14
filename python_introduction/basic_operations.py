ddb94bcc6448e44f7a345caf0d21f3aeb4c7e82a
Wilson Ondiege <willyondiege@gmail.com>
2025-12-14 22:20:05 +0300

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+print("The sum of", number1, "and", number2, "is:", sum_result)
+# This code performs a basic addition operation between two numbers and prints the result.
+diff_result = number1 - number2
+print("The difference between", number1, "and", number2, "is:", diff_result)
+# This code performs a basic subtraction operation between two numbers and prints the result.
+prod_result = number1 * number2
+print("The product of", number1, "and", number2, "is:", prod_result)
+# This code performs a basic multiplication operation between two numbers and prints the result.
